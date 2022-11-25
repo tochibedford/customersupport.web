ae83050a9e10c7b98dd56398aa2aa64defd85b7c
Baribeop Barry Leele <68976451+Baribeop@users.noreply.github.com>
2022-11-25 23:47:11 +0100

    Added email column to Agent model

--- backend/app/models.py
+++ backend/app/models.py
@@ -49,6 +49,7 @@ class Agent(Base):
     id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String, index=True)
     last_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
     company_id = Column(Integer, ForeignKey("companies.id"))
 
     audios = relationship("Audio")
