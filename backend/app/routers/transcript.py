822d442d3ab6a861b435df23b08460e0dffd064a
Tamunonengiyeofori <tkennwariso@gmail.com>
2022-11-25 07:18:32 +0100

    feat(BAC-12):removed response model from get transcript by Id endpoint

--- backend/app/routers/transcript.py
+++ backend/app/routers/transcript.py
@@ -18,7 +18,7 @@ transcript_router = APIRouter(
 #     If the transcript table is available, the code will be refractored to implement changes and queries to the transcript table """
 
 # ENDPOINT TO GET A PARTICULAR TRANSCRIPT USING THE AUDIO ID
-@transcript_router.get("/{job_id}", response_model=schema.Job, description="Retrieving transcript by audio ID")
+@transcript_router.get("/{job_id}", description="Retrieving transcript by audio ID")
 def view_transcript(job_id: Union[int, str], db: Session = Depends(_services.get_session)):
     Job = db.query(models.Job).filter(models.Job.id == job_id).first()
     job_audio_id = Job.audio_id
